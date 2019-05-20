d76492a291edaaa8401c9a316b794b4dde2bc93a
Dan-Vy Le <danvyle85@gmail.com>
2019-05-20 23:27:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,25 +8,3 @@ def my_collect(collection)
     new_collection
 end
 
-
-# def my_collect(array) # put argument(s) here
-#   # code here
-#   i = 0
-#   result = []
-
-#   while i < array.size
-#     block_given? # optional
-#     result << yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
-#     i += 1
-#   end
-# result
-# end
-
-# def my_each(array)
-#   i = 0 
-#   while i < array.length
-#     yield array[i]
-#     i += 1
-#   end
-#   array
-# end
