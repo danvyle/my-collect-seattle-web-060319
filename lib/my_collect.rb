98d5c33c01891b9b7c1408f2d7d75f24ea87245d
Dan-Vy Le <danvyle85@gmail.com>
2019-05-20 23:16:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
   i = 0 
   while i < collection.length
-  new_collection = []
-  new_collection << yield collection[i].upcase
-  i += 1
+    new_collection = []
+    new_collection << yield collection[i].upcase
+    i += 1
   end 
-  new_collection
+    new_collection
 end
 
 # def my_each(array)
